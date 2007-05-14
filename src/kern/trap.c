bec374e56fab48d931805140b9a63e441ff0f579
Luiz Fernando N Capitulino <lcapitulino@gmail.com>
2007-05-14 11:22:03 -0300

    lab3: Panic if a page fault happen in kernel mode
    First part of lab3's Exercise 9 solution.
    
    Signed-off-by: Luiz Fernando N Capitulino <lcapitulino@gmail.com>

--- src/kern/trap.c
+++ src/kern/trap.c
@@ -215,6 +215,8 @@ page_fault_handler(struct Trapframe *tf)
 	// Handle kernel-mode page faults.
 	
 	// LAB 3: Your code here.
+	if (tf->tf_cs == GD_KT)
+		panic("Page fault in kernel mode: 0x%08x\n", fault_va);
 
 	// We've already handled kernel-mode exceptions, so if we get here,
 	// the page fault happened in user mode.
