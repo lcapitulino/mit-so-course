32271572084424f8cb2b7e095750353a9a998a57
Luiz Fernando N. Capitulino <lcapitulino@gmail.com>
2007-11-11 17:41:17 -0200

    Merge lab5 files and changes
    Signed-off-by: Luiz Fernando N. Capitulino <lcapitulino@gmail.com>

--- src/kern/trap.c
+++ src/kern/trap.c
@@ -178,6 +178,9 @@ trap_dispatch(struct Trapframe *tf)
 	if (tf->tf_trapno == IRQ_OFFSET)
 		sched_yield();
 
+	// Handle keyboard interrupts.
+	// LAB 5: Your code here.
+
 	// Unexpected trap: The user process or the kernel has a bug.
 	print_trapframe(tf);
 	if (tf->tf_cs == GD_KT)
